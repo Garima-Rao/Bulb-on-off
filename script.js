959d625f01c5e7d6128ea82b757122bd8918b899
Garima Rao <146614690+Garima-Rao@users.noreply.github.com>
2023-12-19 22:32:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+    function bulb_off(){
+        document.getElementById("image").src="/PROJECTS/lightning-bulb/New folder/kisspng-incandescent-light-bulb-led-lamp-light-emitting-di-a-light-bulb-5a6a5286db2499.3081099115169173828976.png";
+    }
+     function bulb_on(){
+        document.getElementById("image").style.color="orange";
+        document.getElementById("image").src="/PROJECTS/lightning-bulb/New folder/light-bulb-png-bulb-png1247-12.png";
+
+    } 
